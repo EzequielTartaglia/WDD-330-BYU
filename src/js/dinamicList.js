9de4d4e921ff8ca7e1f61029d88aec2e67b43de5
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-05-04 19:50:55 -0300

    Search dinamically

--- src/js/dinamicList.js
+++ src/js/dinamicList.js
@@ -1,18 +1,52 @@
 import ProductData from './ProductData.mjs';
 
+// Get the search term from the URL
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const searchTerm = urlParams.get('search'); // Get the value of the 'search' parameter
+
 // Get the parent element where you want to add the product cards
 const productList = document.getElementById('productsList');
 
-// Create an instance of ProductData and fetch the products
-const productsData = new ProductData('tents');
+// Create an instance of ProductData and fetch products based on the search term
+const productsData = new ProductData(searchTerm);
 
 productsData.getData().then(data => {
-    const products = data; // Assign the result of the promise to the variable products
+    
+    let products;
+
+    switch (searchTerm) {
+        case 'tents':
+            products = data;
+            break;
+
+        case 'backpacks':
+            products = data.Result;
+            break;
+
+        case 'sleeping-bags':
+            products = data.Result;
+            break;
+
+        case 'hammocks':
+            products = data.Result;
+            break;
+            
+        default:
+            products = data;
+            break;
+    }
+
 
     // Create an HTML string representing all the product cards
     const productCardsHTML = products.map(product => {
-        // Determine the image source based on the host
-        const imageSource = window.location.hostname === 'localhost' ? product.Image : product.ImageProduction;
+        // Determine the image source based on the host and search term
+        let imageSource;
+        if (window.location.hostname === 'localhost') {
+            imageSource = searchTerm !== 'tents' ? product.Images.PrimaryMedium : product.Image;
+        } else {
+            imageSource = searchTerm !== 'tents' ? product.Images.PrimaryMedium : product.ImageProduction;
+        }
 
         return `
             <li class="product-card" id="${product.Id}">
@@ -31,4 +65,3 @@ productsData.getData().then(data => {
 }).catch(error => {
     //console.error('Error fetching product data:', error);
 });
-
