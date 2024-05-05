a1e862d432d2e00a911435b71e89e51debc759b8
adriankippins <robertokippins@iotgy.net>
2024-05-04 23:44:24 -0400

    WK 2 Final Push

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+// Define the category
+const category = "Tents";
+
+// Create a new ProductData instance with the category
+const dataSource = new ProductData(category);
+
+// Get the element where the product list will be rendered
+const productListElement = document.getElementById("productsList");
+
+// Instantiate ProductList with the category and product data source
+const productList = new ProductList(category);
+
+// Initialize the product list
+productList.init();
