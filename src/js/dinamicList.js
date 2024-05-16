273907c7d571c107dce58124fd93e19456261eab
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-05-16 14:11:33 -0300

    Force fix

--- src/js/dinamicList.js
+++ src/js/dinamicList.js
@@ -197,8 +197,8 @@ function createProductCardHTML(product) {
               <h3 class="card__brand">${product.Brand.Name}</h3>
               <h2 class="card__name">${product.Name}</h2>
               <p class="product-card__price">$${product.ListPrice}</p>
-              <button id="addToCart">Add to Cart</button>
           </a>
+          <button id="addToCart">Add to Cart</button>
       </li>
   `;
 }
